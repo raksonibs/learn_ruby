7193597a3c2d5bb5dd85b18e5c1cd0278ed9bbae
Oskar Niburski <oskarniburski@gmail.com>
2013-10-23 14:50:33 -0400

    Calculator functioning with bonus questions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,3 +9,15 @@ end
 def sum(arr)
 	arr.size==0 ? 0 : arr.inject(:+)
 end
+
+def multiply(*arr)
+	arr.inject(:*)
+end
+
+def factorial(base)
+	if base==1 || base==0
+		return 1
+	else
+		base*factorial(base-1)
+	end
+end

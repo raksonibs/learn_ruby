dfb02a92cca711646f828ee740fd04a41018e9e5
Oskar Niburski <oskarniburski@gmail.com>
2013-10-23 20:47:55 -0400

    Configured timer via simple math, functional now

--- 08_book_titles/timer.rb
+++ 08_book_titles/timer.rb
@@ -5,12 +5,20 @@ class Timer
 	end
 
 	def time_string
-		minutes="#{@seconds/60.0}"
-		minutesfract=((((minutes[/.\w+/]).to_f*60).round.to_s))
-		total=minutes[/\w./]+minutesfract
+		if @seconds==0
+			"00:00:00"
+		else
+			val=[]
+			str=""
+			val <<(@seconds/3600).to_s
+			val <<((@seconds%3600)/60).to_s
+			val << ((@seconds%3600)%60).to_s
+			arr=val.map {|i| i.size==1 ? "0"+ i : i}
+			arr.each do |i|
+				str+=i
+				str+=":" unless arr.last==i
+			end
+			str
+		end
 	end
 end
-
-dog=Timer.new
-dog.seconds=634
-puts dog.time_string

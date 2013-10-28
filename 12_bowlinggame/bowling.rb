98dcfc6b11e705efead8c83b0ae76e446e85a998
Oskar Niburski <oskarniburski@gmail.com>
2013-10-27 23:45:16 -0400

    Bowling score now working, accounts for spares!

--- 12_bowlinggame/bowling.rb
+++ 12_bowlinggame/bowling.rb
@@ -1,6 +1,42 @@
 class Game
-	def score
+	def score(n)
+		#10 is one turn, and if there are any tens the bonus is 
+		#the value of te next two balls rolled
+		spares=[]
+		spare=0 #will record next pin if saw spare (two make ten), then add to score
+		strikes=[]
+		total=0 # will record next ten pins if saw a ten
+		n.each_with_index do |val,i|
+			if val==10
+				unless i==9 || i==10 || i==11
+					strikes << n[i+1] 
+					strikes << n[i+2]
+				end
+				total+=val
+
+			else
+
+				spare+=val
+
+
+				if i%2!=0
+
+					if spare==10
+						spares << n[i+1] unless i==19
+
+					end
+					spare=0
+				end
+				total+=val
+			end
+		end
+	sum=spares.compact.inject(total) {|sum,value| sum+value}
+	strikes.compact.inject(sum) {|s,v| s+v}
+
+
+
+
 	end
 end
 
-p 12.times.map {10}
+
--- 12_bowlinggame/bowling_spec.rb
+++ 12_bowlinggame/bowling_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'bowling'
 
 describe Game do
   let (:game) { Game.new }
